8b3c54e310902f93ba8e0ca55fd0d77f32901bd3
Apricots Ghar <aghar@am-j2.msk.21-school.ru>
2020-08-20 14:59:14 +0300

    fix

--- lastversion/stars_manager.c
+++ lastversion/stars_manager.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stars_manager.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: artemignatev <artemignatev@student.42.f    +#+  +:+       +#+        */
+/*   By: aghar <aghar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/16 02:27:41 by artemignate       #+#    #+#             */
-/*   Updated: 2020/08/16 02:46:04 by artemignate      ###   ########.fr       */
+/*   Updated: 2020/08/20 14:47:12 by aghar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 
 void	stars_manager(t_info *info, va_list arg_list)
 {
+	// int a;
+	// a = va_arg(arg_list, int);
 	if (info->width_mod == 2)
 	{
 		info->width = va_arg(arg_list, int);
+		if (info->width <= 0)
+		{
+			info->width *= -1;
+			info->flag_minus = 1;
+		}
 		info->width_mod = 0;
 	}
 	if (info->precision_mod == 2)
 	{
 		info->precision = va_arg(arg_list, int);
+		if (info->precision <= 0)
+		{
+			info->precision *= -1;
+			info->flag_minus = 1;
+		}
 		info->precision_mod = 0;
 	}
 }
